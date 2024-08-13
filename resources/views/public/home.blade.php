068c6cd7476e74347b41ea61bb55b0fd78f93094
Jack Weston <83405373+WackJeston@users.noreply.github.com>
2024-08-13 22:33:16 +0100

    Noice Progress

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -16,17 +16,33 @@
     @endif
 
 		@if (count($landingZoneBanners) > 0)
-			<div async id="bannerhometop">
-				<bannerhometop 
-					:banners="{{ json_encode($landingZoneBanners) }}"
-					tickets="{{ $ticketsActive }}"
-					ticketdate="{{ $ticketDate }}"
-				/>
+			<div class="banner-zone">
+				<div async id="bannerhometop">
+					<bannerhometop 
+						:banners="{{ json_encode($landingZoneBanners) }}"
+						tickets="{{ $ticketsActive }}"
+						ticketdate="{{ $ticketDate }}"
+					/>
+				</div>
+	
+				@if ($primaryInfo != null)
+					<div class="primary-info-desktop">
+						@if ($primaryInfo->title != null)
+							<h2>{{ $primaryInfo->title }}</h2>
+						@endif
+						@if ($primaryInfo->subtitle != null)
+							<h3>{{ $primaryInfo->subtitle }}</h3>
+						@endif
+						@if ($primaryInfo->description != null)
+							<p>{{ $primaryInfo->description }}</p>
+						@endif
+					</div>
+				@endif
 			</div>
 		@endif
 
 		@if ($primaryInfo != null)
-			<div class="clear-box limited dk">
+			<div class="primary-info-mobile clear-box limited dk">
 				@if ($primaryInfo->title != null)
 					<h2>{{ $primaryInfo->title }}</h2>
 				@endif
@@ -40,7 +56,7 @@
 		@endif
 		
 		{{-- Remove after updates are complete --}}
-		<div style="display: flex; flex-direction: column; align-items: center; margin: 0 40px 50px 40px;">
+		<div style="display: flex; flex-direction: column; align-items: center; margin: 30px 40px 50px 40px;">
 			<img style="display flex; height: 500px;" src="{{env('ASSET_PATH')}}homepage-advert.jpeg" alt="Performance Act Advert">
 		</div>
 
