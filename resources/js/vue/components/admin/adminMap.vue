7e592c4b527712b5d223526e88708f9aa6a3d731
Jack Weston <jack-weston99@protonmail.com>
2024-08-20 17:11:47 +0100

    Some map progress (Draggable issues)

--- resources/js/vue/components/admin/adminMap.vue
+++ resources/js/vue/components/admin/adminMap.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="web-box" id="mapSection">
-		<img :src="this.map.fileName" alt="Map Image" id="mapImage">
+		<div id="mapImageContainer">
+			<img :src="this.map.fileName" alt="Map Image" id="mapImage">
+		</div>
 
 		<div id="mapIconsSection">
 			<h2>Map Icons</h2>
 			<small>Click an icon to add it to the map. Then click and drag to position the icon.</small>
 			<div id="mapIcons">
-				<img v-for="(icon, i) in this.icons" :src="icon.fileName" alt="Map Icon Image" id="mapIcon" @click="this.createIcon($event)">
+				<img v-for="(icon, i) in this.icons" :src="icon.fileName" alt="Map Icon Image" class="mapIcon" @click="this.createIcon($event)">
 			</div>
 		</div>
 	</div>
@@ -22,13 +24,31 @@
 
 		methods: {
 			createIcon(event) {
-				console.log(event.target);
+				let primary = document.getElementById('mapImageContainer');
+				let count = primary.children.length;
 
 				let newDiv = document.createElement('div');
+				newDiv.id = 'icon-' + count;
 				newDiv.style.position = 'absolute';
 				newDiv.style.top = 0;
 				newDiv.style.left = 0;
-			}
+				newDiv.setAttribute('draggable', true);
+
+				let newImg = document.createElement('img');
+				newImg.src = event.target.src;
+				newImg.style.width = '100px';
+				newImg.style.height = '100px';
+
+				newDiv.appendChild(newImg);
+				primary.appendChild(newDiv);
+
+				let newDiv2 = document.getElementById('icon-' + count);
+				newDiv2.addEventListener("dragstart", (event) =>
+					event.dataTransfer.setData("text/plain", "This text may be dragged"),
+				);
+
+				console.log(newDiv2);
+			},
 
 			// saveMap() {
 			// 	let primary = document.getElementById('mapImage');
